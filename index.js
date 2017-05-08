2571b33f71fae07b7c78e6588efed333f2268d94
nitaybz <nitaybz@gmail.com>
2017-05-08 23:55:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -81,30 +81,30 @@ broadlinkMP.prototype.getState = function(callback) {
                     dev.on("s1_power", (s1) => {
                         self.log("s1 power is on " + s1);
                         self.s1_powered = s1;
-                        callback(null, s1);
-                        break;
+                        return callback(null, s1);
                     });
+                    break;
                 case "S2":
                     dev.on("s2_power", (s2) => {
                         self.log("s2 power is on " + s2);
                         self.s2_powered = s2;
-                        callback(null, s2);
-                        break;
+                        return callback(null, s2);
                     });
+                    break;
                 case "S3":
                     dev.on("s3_power", (s3) => {
                         self.log("s3 power is on " + s3);
                         self.s3_powered = s3;
-                        callback(null, s3);
-                        break;
+                        return callback(null, s3);
                     });
+                    break;
                 case "S4":
                     dev.on("s4_power", (s4) => {
                         self.log("s4 power is on " + s4);
                         self.s4_powered = s4;
-                        callback(null, s4);
-                        break;
-                        });
+                        return callback(null, s4);
+                    });
+                    break;
             }
 
         } else {
