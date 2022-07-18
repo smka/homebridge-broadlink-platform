5684ad8c2f7dfaf5499b9599d251294b40ba58a0
Dwan <me@dwaan.com>
2022-07-18 09:47:21 +0300

    Add interval in config, reduce plugins memory footprint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var broadlink = require('broadlinkjs-dw');
 
 const MP = "MP";
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     homebridge.registerPlatform("homebridge-broadlink-platform-outlet", "broadlinkPlatformOutlet", broadlinkPlatform);
@@ -12,24 +12,24 @@ module.exports = function(homebridge) {
 function broadlinkPlatform(log, config, api) {
     this.log = log;
     this.config = config;
-    if(api) this.api = api;
+    if (api) this.api = api;
 }
 
 broadlinkPlatform.prototype = {
-    accessories: function(callback) {
+    accessories: function (callback) {
         //For each device in cfg, create an accessory!
         var accessories = this.config.accessories;
         var accessoriesList = [];
         var length = accessories ? accessories.length : 0;
 
         for (var i = 0; i < length; i++) {
-            if(accessories[i].type == MP) {
+            if (accessories[i].type == MP) {
                 for (var outlet = 1; outlet <= 4; outlet++) {
                     accessories[i].index = outlet;
                     accessories[i].outletName = "S" + outlet;
                     accessories[i].outletTimer = [];
-                    if(accessories[i].timer) 
-                        if(accessories[i].timer[outlet - 1]) 
+                    if (accessories[i].timer)
+                        if (accessories[i].timer[outlet - 1])
                             accessories[i].outletTimer = accessories[i].timer[outlet - 1];
                     accessories[i].interval = accessories[i].interval || 0;
 
@@ -61,15 +61,15 @@ function BroadlinkAccessory(log, config) {
     this.firstTime = true;
     this.onGetState = false;
     this.onSetState = false;
-    
-    if(!this.mac) throw new Error("You must provide a config value for 'mac' address.");
+
+    if (!this.mac) throw new Error("You must provide a config value for 'mac' address.");
 
     // MAC string to MAC buffer
-    this.mac_buff = function(mac) {
-        var mb = new Buffer.alloc(6,0);
-        if(mac) {
+    this.mac_buff = function (mac) {
+        var mb = new Buffer.alloc(6, 0);
+        if (mac) {
             var values = mac.split(':');
-            if(!values || values.length != 6) {
+            if (!values || values.length != 6) {
                 throw new Error('Invalid MAC [' + mac + ']; should follow pattern ##:##:##:##:##:##');
             }
             for (var i = 0; i < values.length; ++i) {
@@ -83,7 +83,7 @@ function BroadlinkAccessory(log, config) {
 }
 
 BroadlinkAccessory.prototype = {
-    getServices: function() {
+    getServices: function () {
         var type = this.config.type;
         var services = [];
 
@@ -99,7 +99,7 @@ BroadlinkAccessory.prototype = {
 
         this.informationService
             .setCharacteristic(Characteristic.Model, type)
-            .setCharacteristic(Characteristic.SerialNumber, '1.0');
+            .setCharacteristic(Characteristic.SerialNumber, '1.0.5');
 
         services.push(this.switchService, this.informationService);
 
@@ -109,8 +109,7 @@ BroadlinkAccessory.prototype = {
         return services;
     },
 
-    // b: broadlink
-    discover: function(broadlink) {
+    discover: function (broadlink) {
         try {
             broadlink.discover();
         } catch (error) {
@@ -118,23 +117,23 @@ BroadlinkAccessory.prototype = {
             this.onSetState = false;
         }
     },
-    
+
     // Get MP/SP outlets status
-    getState: function(callback) {
+    getState: function (callback) {
         var device = new broadlink();
-        
+
         this.onGetState = true;
         this.discover(device);
         device.on("deviceReady", (dev) => {
-            if(this.mac_buff(this.mac).equals(dev.mac) && !this.onSetState) {
+            if (this.mac_buff(this.mac).equals(dev.mac) && !this.onSetState) {
                 dev.check_power();
                 dev.on(this.type == MP ? "mp_power" : "power", (statuses) => {
                     var status = (this.type == MP ? statuses[this.index - 1] : statuses) ? true : false;
-                    
+
                     dev.exit();
                     this.onGetState = false;
-    
-                    if(this.firstTime || this.powered != status) {
+
+                    if (this.firstTime || this.powered != status) {
                         this.powered = status ? true : false;
                         this.firstTime = false;
                         this.switchService.updateCharacteristic(Characteristic.On, this.powered);
@@ -142,17 +141,18 @@ BroadlinkAccessory.prototype = {
                     }
                 });
             } else {
-                dev.exit();
+                device = null;
                 this.onGetState = false;
             }
 
+            device = null;
         });
 
         callback(null, this.powered);
     },
 
     // Set MP/SP outlets status
-    setState: function(state, callback) {
+    setState: function (state, callback) {
         var device = new broadlink();
         var checkAgain;
 
@@ -160,27 +160,27 @@ BroadlinkAccessory.prototype = {
         state = state ? true : false;
 
         this.log(`[${this.name}] 👈`);
-        
+
         this.discover(device);
         checkAgain = setInterval(() => {
             this.discover(device);
         }, 1000);
-        
-        device.on("deviceReady", (dev) => {            
-            if(this.mac_buff(this.mac).equals(dev.mac)) {
+
+        device.on("deviceReady", (dev) => {
+            if (this.mac_buff(this.mac).equals(dev.mac)) {
                 // Set the power
                 this.powered = state;
-                if(this.type == MP) dev.set_power(this.index, this.powered);
+                if (this.type == MP) dev.set_power(this.index, this.powered);
                 else dev.set_power(this.powered);
                 this.logState();
-                
+
                 // Check status while making sure state changed
                 dev.check_power();
-                dev.on(this.type == MP ? "mp_power" : "power", (statuses) => {                
-                    if(this.mac_buff(this.mac).equals(dev.mac)) {
+                dev.on(this.type == MP ? "mp_power" : "power", (statuses) => {
+                    if (this.mac_buff(this.mac).equals(dev.mac)) {
                         var status = (this.type == MP ? statuses[this.index - 1] : statuses) ? true : false;
 
-                        if(status != state) this.switchService.setCharacteristic(Characteristic.On, this.powered);
+                        if (status != state) this.switchService.setCharacteristic(Characteristic.On, this.powered);
 
                         // Stop the set loop
                         this.onSetState = false;
@@ -191,25 +191,28 @@ BroadlinkAccessory.prototype = {
                 clearInterval(checkAgain);
                 dev.exit();
                 callback(null);
-    }
+            }
+
+            device = null;
         });
     },
 
     // Update outlets status based on the last status
-    updateState: function() {
+    updateState: function () {
         setInterval(() => {
-            if(!this.onGetState && !this.onSetState) this.getState(() => { });
+            if (!this.onGetState && !this.onSetState) this.getState(() => { });
+            // this.log(`[${this.name}] - On get: ${this.onGetState}, On set: ${this.onSetState}`)
         }, this.interval);
     },
 
-    logState: function(extra, tail) {
+    logState: function (extra, tail) {
         var message = `${extra ? extra + ": " : ""}${this.bulb(this.powered)} [${this.name}] ${tail ? "-> " + tail : ""}`;
 
-        if(message != this._message) this.log(message);
+        if (message != this._message) this.log(message);
         this._message = message;
     },
-    
-    bulb: function(state) {
-        return state? "🟡": "⚪️";
+
+    bulb: function (state) {
+        return state ? "🟡" : "⚪️";
     }
 }
